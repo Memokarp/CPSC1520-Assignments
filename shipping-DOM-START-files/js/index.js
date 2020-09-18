9d510566965f817020b5113a1a6817d984a5ca63
Memokarp <67663360+Memokarp@users.noreply.github.com>
2020-09-18 13:52:33 -0600

    Class 3 - Week 2

---
+++ shipping-DOM-START-files/js/index.js
@@ -0,0 +1,22 @@
+/* 
+    document.getElementById('id-to-select')
+    return Type:element/group of elements markup
+
+    document.getElementByClassName('test')
+    return Type:HTMLCollection
+
+    document.getElementByTagName('img')
+    return Type:HTMLCollection
+
+    document.querySelector('test')
+    return Type:HTML Element, Markup
+
+    document.querySelectorAll('.test')
+    return Type:Nodelist
+
+     <img src="img/truck.svg" alt="shipping van">
+*/
+
+console.log(document.getElementsByClassName('truck'))
+
+
